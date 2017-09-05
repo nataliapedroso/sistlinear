56ec59b2be92a97941ab7d1541294d14c8c61fa7
natalia pedroso <nataliapedroso@id.uff.br>
2017-09-05 15:04:45 -0300

    main e declaração de variaveis

--- sistlinear.c
+++ sistlinear.c
@@ -19,3 +19,9 @@ void imprime(float v1[], float v2[], float v3[], float v4[])
 		printf(" %.2f\t", v4[i]);
 	printf("\n");
 }
+main()
+{
+	int i;
+	float K, x1, x2, x3, x4, m1[5], m2[5], m3[5], m4[5], ma[5];
+
+	
