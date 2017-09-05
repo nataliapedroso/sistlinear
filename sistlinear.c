2c9025348847dda0320cbb8bd24597533bb5b113
natalia pedroso <nataliapedroso@id.uff.br>
2017-09-05 15:20:43 -0300

    escalonou a segunda coluna

--- sistlinear.c
+++ sistlinear.c
@@ -22,7 +22,7 @@ void imprime(float v1[], float v2[], float v3[], float v4[])
 main()
 {
 	int i;
-	float K, x1, x2, x3, x4, m1[5], m2[5], m3[5], m4[5], ma[5];
+	float K, x1, x2, x3, x4, m1[5], m2[5], m3[5], m4[5], maux[5];
 
 	printf("\n Informe os valores da primeira linha da matriz: \n");
 	for(i=0; i<5; i++)
@@ -66,11 +66,11 @@ main()
 	else
 	{
 		for(i=0; i<5; i++)
-			ma[i]=m1[i];
+			maux[i]=m1[i];
 		for(i=0; i<5; i++)
 		{
 			m1[i]=m2[i];
-			m2[i]=ma[i];
+			m2[i]=maux[i];
 		}
 	}
 
@@ -94,4 +94,30 @@ main()
 
 	imprime(m1,m2,m3,m4);
 
-	
+	if(m2[1]!=0)
+	{
+		K=(m3[1]/m2[1]);
+		for(i=0; i<5; i++)
+			m3[i]=m3[i]-K*m2[i];
+	}
+	else
+	{
+		for(i=0; i<5; i++)
+			maux[i]=m2[i];
+		for(i=0; i<5; i++)
+		{
+			m2[i]=m3[i];
+			m3[i]=maux[i];
+		}
+	}
+
+	imprime(m1,m2,m3,m4);
+
+	if(m4[1]!=0)
+	{
+		K=(m4[1]/m2[1]);
+		for(i=0; i<5; i++)
+			m4[i]=m4[i]-K*m2[i];
+	}
+
+	imprime(m1,m2,m3,m4);
