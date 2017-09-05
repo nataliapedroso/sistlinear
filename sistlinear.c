87a5f0bd02483afc01abd30aa5ec447b8686323f
natalia pedroso <nataliapedroso@id.uff.br>
2017-09-05 15:08:30 -0300

    leitura da matriz

--- sistlinear.c
+++ sistlinear.c
@@ -24,4 +24,31 @@ main()
 	int i;
 	float K, x1, x2, x3, x4, m1[5], m2[5], m3[5], m4[5], ma[5];
 
-	
+	printf("\n Informe os valores da primeira linha da matriz: \n");
+	for(i=0; i<5; i++)
+	{
+		printf(" %dº: ", i+1);
+		scanf("%f", &m1[i]);
+	}
+
+	printf("\n Segunda linha: \n");
+	for(i=0; i<5; i++)
+	{
+		printf(" %dº: ", i+1);
+		scanf("%f", &m2[i]);
+	}
+
+	printf("\n Terceira linha: \n");
+	for(i=0; i<5; i++)
+	{
+		printf(" %dº: ", i+1);
+		scanf("%f", &m3[i]);
+	}
+
+	printf("\n Quarta linha: \n");
+	for(i=0; i<5; i++)
+	{
+		printf(" %dº: ", i+1);
+		scanf("%f", &m4[i]);
+	}
+
