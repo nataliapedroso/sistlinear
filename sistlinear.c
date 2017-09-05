558b52e7a4df30a696ae2485d6346ca9710d7f5f
natalia pedroso <nataliapedroso@id.uff.br>
2017-09-05 15:03:30 -0300

    criação e função imprime

--- sistlinear.c
+++ sistlinear.c
@@ -1,28 +1,21 @@
 #include<stdio.h>
-void imprime(float mat[4][5])
+void imprime(float v1[], float v2[], float v3[], float v4[])
 {
-	int l, c;
-	for(l=0; l<4; l++)
-	{
-		for(c=0; c<5; c++)
-			printf(" %.2f\t", mat[l][c]);
-		printf("\n");
-	}
+	int i;
+
+	for(i=0; i<5; i++)
+		printf(" %.2f\t", v1[i]);
+	printf("\n");
+
+	for(i=0; i<5; i++)
+		printf(" %.2f\t", v2[i]);
+	printf("\n");
+
+	for(i=0; i<5; i++)
+		printf(" %.2f\t", v3[i]);
+	printf("\n");
+
+	for(i=0; i<5; i++)
+		printf(" %.2f\t", v4[i]);
+	printf("\n");
 }
-main()
-{
-	int l, c;
-	float m1[4][5];
-
-	printf("\n Informe os elementos da matriz: \n");
-	for(l=0; l<4; l++)
-	{
-		for(c=0; c<5; c++)
-		{
-			printf(" [%d][%d]: ", l, c);
-			scanf("%f", &m1[l][c]);
-		}
-	}
-
-	imprime(m1[4][5]);
-
